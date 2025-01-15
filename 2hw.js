fd3033d5837be01a54f5337652d6863851ceb4a9
AndreyZykin <andrei.zykin3077@gmail.com>
2025-01-15 23:35:09 +0400

    Закончил домашнюю работу

---
+++ 2hw.js
@@ -0,0 +1,44 @@
+
+let a = 10;
+alert (a);
+alert (a = 20);
+
+let iphone =  2007;
+alert (iphone);
+
+let js = "Брендан Эйх";
+alert (js);
+
+let x = 10;
+let y = 2;
+alert (x + y);
+alert (x - y);
+alert (x * y);
+alert (x / y);
+
+alert (y ** 5);
+
+a = 9
+b = 2
+alert (a % b);
+
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+
+let age = Number( prompt("Сколько вам лет?"));
+alert (age);
+
+let user = {
+    name: "Андрей",    
+    age: 34,            
+    isAdmin: true };
+    console.log(user);
+
+let yourName = prompt("Как вас зовут?");
+alert (yourName); 
