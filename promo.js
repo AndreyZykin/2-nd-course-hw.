208fc1f8f907e26165f875cebbc01291ad6f5aaf
Андрей Зыкин <andrei.zykin3077@gmail.com>
2025-03-27 01:03:00 +0400

    Исправил адаптив мобилки

---
+++ promo.js
@@ -0,0 +1,27 @@
+let product = 'привет';
+
+let number = 10;
+
+let a = false;
+
+number = 42;
+
+product ='спокойной ночи';
+
+if (number === 42){
+    alert (product)
+}
+else {
+alert ('нет')
+}
+
+function game () {
+    console.log ('камень,ножницы,бумага')
+}
+
+const site = () =>{
+
+}
+function getFullName (name, surname) {
+    return name +' '+ surname
+} 
