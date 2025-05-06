766c4e1ed63375057a87db3c5ddda73342fd3a1a
Андрей Зыкин <andrei.zykin3077@gmail.com>
2025-05-06 15:12:20 +0400

    добавил игру исправленную

--- hw6.js
+++ hw6.js
@@ -1,38 +1,78 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const startButton = document.getElementById('startButton');
-
-    startButton.addEventListener('click', function() {
-        playGame();
-    });
-});
-
-
-function playGame() {
+function generateTask() {
     const operators = ['+', '-', '*', '/'];
-    const num1 = Math.floor(Math.random() * 10) + 1;
-    const num2 = Math.floor(Math.random() * 10) + 1;
     const operator = operators[Math.floor(Math.random() * operators.length)];
-    let correctAnswer;
+    const num1 = Math.floor(Math.random() * 20) + 1;
+    const num2 = Math.floor(Math.random() * 20) + 1;
+
+    let task;
+    let result;
 
     switch (operator) {
         case '+':
-            correctAnswer = num1 + num2;
+            task = `${num1} + ${num2}`;
+            result = num1 + num2;
             break;
         case '-':
-            correctAnswer = num1 - num2;
+            task = `${num1} - ${num2}`;
+            result = num1 - num2;
             break;
         case '*':
-            correctAnswer = num1 * num2;
+            task = `${num1} * ${num2}`;
+            result = num1 * num2;
             break;
         case '/':
-            correctAnswer = num1 / num2;
+            task = `${num1} / ${num2}`;
+            result = num1 / num2;
             break;
     }
 
-    const userAnswer = prompt(`${num1} ${operator} ${num2} = ?`);
-    if (parseFloat(userAnswer) === correctAnswer) {
+    return { task, result };
+}
+
+// Основная функция игры
+function playGame2() {
+    const { task, result } = generateTask();
+    const userAnswer = prompt(`Решите задачу: ${task}`);
+
+    if (userAnswer === null) {
+        alert('Игра отменена.');
+        return;
+    }
+
+    const parsedAnswer = parseFloat(userAnswer);
+
+    if (isNaN(parsedAnswer)) {
+        alert('Пожалуйста, введите числовое значение.');
+        playGame2(); // Повторный вызов игры
+        return;
+    }
+
+    if (parsedAnswer === result) {
         alert('Верно!');
     } else {
-        alert(`Неверно. Правильный ответ: ${correctAnswer}`);
+        alert(`Ошибка! Правильный ответ: ${result}`);
     }
 }
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.preview-game__button').addEventListener('click', playGame);
+});
+
+const array = [1, 5, 4, 10, 0, 3];
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] === 10) {
+    console.log(array[i]);
+    break; // Прерываем цикл, когда встречается значение 10
+  }
+  console.log(array[i]);
+}
+
+const arr1 = [1, 5, 4, 10, 0, 3];
+
+const index = arr.indexOf(4);
+console.log(index);
+
+const arr2 = [1, 3, 5, 10, 20];
+
+const result = arr.join(' ');
+console.log(result);
