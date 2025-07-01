469c3ed46d531c17711af877301c7d3f34db7e89
Андрей Зыкин <andrei.zykin3077@gmail.com>
2025-07-02 00:06:33 +0400

    убрал лишние файлы с js

--- hw10.js
+++ hw10.js
@@ -1,13 +1,14 @@
- // Генерация случайного HEX цвета
-        function getRandomColor() {
-            // (0..255) для R, G, B, переводим в двухсимвольный HEX и склеиваем
-            return "#" + 
-                Math.floor(Math.random()*256).toString(16).padStart(2, "0") +
-                Math.floor(Math.random()*256).toString(16).padStart(2, "0") +
-                Math.floor(Math.random()*256).toString(16).padStart(2, "0");
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    // Генерация случайного HEX цвета
+    function getRandomColor() {
+        return "#" + 
+            Math.floor(Math.random() * 256).toString(16).padStart(2, "0") +
+            Math.floor(Math.random() * 256).toString(16).padStart(2, "0") +
+            Math.floor(Math.random() * 256).toString(16).padStart(2, "0");
+    }
 
-        // Обработчик нажатия на кнопку
-        document.getElementById('colorBtn').addEventListener('click', function() {
-            document.body.style.background = getRandomColor();
-        });
+    // Обработчик нажатия на кнопку
+    document.getElementById('colorBtn').addEventListener('click', function() {
+        document.body.style.background = getRandomColor();
+    });
+});
